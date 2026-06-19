2224fe022aadd1c1b5644d95d9a4f9c957c79c45
mmcky <mmcky@users.noreply.github.com>
2026-06-19 01:10:10 +0000

    Deploy preview for PR 97 🛫

---
+++ pr-preview/pr-97/build/_shared/chunk-7PSSNOFQ.js
@@ -0,0 +1,15 @@
+import{b as e}from"/quantecon-theme.mystmd/pr-preview/pr-97/build/_shared/chunk-DZ66R7FY.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as a};
---
+++ pr-preview/pr-97/build/_shared/chunk-MTZPJXI5.js
@@ -0,0 +1,1 @@
+import{b as t}from"/quantecon-theme.mystmd/pr-preview/pr-97/build/_shared/chunk-DZ66R7FY.js";var s=class{constructor(i){this.init=i,this.records=this.init()}static{t(this,"ImperativeState")}reset(){this.records=this.init()}};export{s as a};
